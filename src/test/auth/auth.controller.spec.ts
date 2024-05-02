8fe48682f6a2f3931f0d714c38e1ecdd45afea0e
leemhoon00 <leemhoon000@gmail.com>
2024-05-03 00:47:56 +0900

    test: refresh - auth.controller [Unit Test]

--- src/test/auth/auth.controller.spec.ts
+++ src/test/auth/auth.controller.spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
 import { ConfigService } from '@nestjs/config';
-import { KakaoRequest } from 'src/auth/requests';
+import { KakaoRequest, JwtRequest } from 'src/auth/requests';
 import { Response } from 'express';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
   let authController: AuthController;
@@ -19,6 +20,7 @@ describe('AuthController', () => {
           provide: AuthService,
           useValue: {
             login: jest.fn(),
+            refresh: jest.fn(),
           },
         },
       ],
@@ -65,6 +67,70 @@ describe('AuthController', () => {
         ...cookieOptions,
         httpOnly: false,
       });
+    });
+  });
+
+  describe('refresh', () => {
+    it('새로운 액세스 토큰을 쿠키에 설정한다', async () => {
+      // given
+      const req = {
+        cookies: {
+          refreshToken: 'refreshToken',
+        },
+      } as unknown as JwtRequest;
+      const res = { cookie: jest.fn(), send: jest.fn() } as unknown as Response;
+      authService.refresh.mockResolvedValue('newAccessToken');
+
+      // when
+      await authController.refresh(req, res);
+
+      // then
+      const cookieOptions = {
+        httpOnly: true,
+        secure: true,
+        domain: configService.get('COOKIE_DOMAIN'),
+      };
+      expect(res.cookie).toHaveBeenCalledTimes(1);
+      expect(res.cookie).toHaveBeenCalledWith(
+        'accessToken',
+        'newAccessToken',
+        cookieOptions,
+      );
+    });
+
+    it('액세스 토큰 재발급 실패 시 쿠키를 삭제하고 401 에러를 던진다', async () => {
+      // given
+      const req = {
+        cookies: { refreshToken: 'refreshToken' },
+      } as unknown as JwtRequest;
+      const res = { clearCookie: jest.fn() } as unknown as Response;
+      authService.refresh.mockRejectedValue(new Error());
+
+      // when
+      try {
+        await authController.refresh(req, res);
+      } catch (err) {
+        // then
+        const cookieOptions = {
+          httpOnly: true,
+          secure: true,
+          domain: configService.get('COOKIE_DOMAIN'),
+        };
+        expect(res.clearCookie).toHaveBeenCalledTimes(3);
+        expect(res.clearCookie).toHaveBeenCalledWith(
+          'accessToken',
+          cookieOptions,
+        );
+        expect(res.clearCookie).toHaveBeenCalledWith(
+          'refreshToken',
+          cookieOptions,
+        );
+        expect(res.clearCookie).toHaveBeenCalledWith('isLoggedIn', {
+          ...cookieOptions,
+          httpOnly: false,
+        });
+        expect(err).toBeInstanceOf(UnauthorizedException);
+      }
     });
   });
 });
